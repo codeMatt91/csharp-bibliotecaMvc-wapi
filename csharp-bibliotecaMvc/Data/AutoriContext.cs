471d063a4b2f0ab15cd93f3dc5e3c8a0b9941f92
Matteo Imbimbo <matteo.imbimbo@yahoo.it>
2022-06-15 16:20:12 +0200

    Aggiungere i file di progetto.

---
+++ csharp-bibliotecaMvc/Data/AutoriContext.cs
@@ -0,0 +1,14 @@
+﻿using csharp_bibliotecaMvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace csharp_bibliotecaMvc.Data
+{
+    public class AutoriContext : DbContext
+    {
+        public AutoriContext(DbContextOptions<AutoriContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Autori> Autoris { get; set; }
+    }
+}
